bd9a3e0c0dfb1a233888c2040d5945259e90a10c
Leonid Sklyut <lsklyut@kbra.com>
2018-03-21 18:02:33 -0400

    demonstrate phpexcel / phpspreadsheet bug

---
+++ bug.php
@@ -0,0 +1,29 @@
+<?php
+
+require 'vendor/autoload.php';
+
+use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+
+$spreadsheet = new Spreadsheet();
+$sheet = $spreadsheet->getActiveSheet();
+$sheet->setTitle('Sheet 1');
+$sheet->setCellValue('A1', 'Hello World !');
+
+$secondSheet = $spreadsheet->createSheet();
+$secondSheet->setTitle('Sheet 2');
+
+$secondSheet->setCellValue('A1', '=\'Sheet 1\'!A1');
+
+var_dump($secondSheet->toArray());
+
+$sheet->setCellValue('A1', 'Turkey');
+
+var_dump($secondSheet->toArray());
+
+$sheet->setCellValue('A1', 'Pizza');
+
+var_dump($secondSheet->toArray());
+
+$writer = new Xlsx($spreadsheet);
+$writer->save('hello world.xlsx');
